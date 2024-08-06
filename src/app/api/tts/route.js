5b7af35dbd421c7b1009344dca87c1ceee2c134e
Kevin Hou <strawberrypi64@gmail.com>
2024-08-06 18:56:06 +1000

    chore: api setup
    - /api/tts api route
    - receives JSON and returns response

---
+++ src/app/api/tts/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+export const maxDuration = 60;
+
+export async function POST(req) {
+  if (req.method !== "POST") {
+    return NextResponse.json({ error: "POST ONLY" }, { status: 405 });
+  }
+
+  try {
+    const { text } = await req.json();
+
+    const response = {};
+
+    return NextResponse.json({ response: response });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
